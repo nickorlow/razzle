ce2b37331399b0e661063a8fab4d19fb9bb0928c
Nicholas Orlowsky <nickorlow@nickorlow.com>
2025-02-25 16:24:07 -0500

    add nix config

---
+++ shell.nix
@@ -0,0 +1,10 @@
+let
+  pkgs' = (import <nixpkgs> {}).pkgsCross.i686-embedded;
+  pkgs = (import <nixpkgs> {});
+in
+  pkgs'.mkShell {
+    nativeBuildInputs = [ 
+        pkgs.cdrkit
+        pkgs.qemu
+    ];
+}
